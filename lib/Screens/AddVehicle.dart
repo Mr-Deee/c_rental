34b7c64e78af3fe6255177a8fa4b2650a99f3251
DANIEL <merchantdaniel8@gmail.com>
2024-04-01 22:38:42 +0000

    fix2

---
+++ lib/Screens/AddVehicle.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class AddVehicle extends StatefulWidget {
+  const AddVehicle({super.key});
+
+  @override
+  State<AddVehicle> createState() => _AddVehicleState();
+}
+
+class _AddVehicleState extends State<AddVehicle> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
