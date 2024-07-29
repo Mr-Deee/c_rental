a0fb2ce4e87ff0022846b353534fbca9a5452162
mr-deeee <merchantdaniel8@gmail.com>
2024-07-29 23:02:41 +0000

    deleting some [apages

--- lib/Screens/payment and Users.dart
+++ lib/Screens/payment and Users.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class PaymentandUsers extends StatefulWidget {
   const PaymentandUsers({super.key});
@@ -8,8 +10,117 @@ class PaymentandUsers extends StatefulWidget {
 }
 
 class _PaymentandUsersState extends State<PaymentandUsers> {
+
+  bool showUsers = true;
+  final DatabaseReference _databaseReference = FirebaseDatabase.instance.reference();
+  List<Map<String, dynamic>> users = [];
+  List<Map<String, dynamic>> payments = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchData();
+  }
+
+  Future<void> _fetchData() async {
+    DatabaseEvent usersSnapshot = await _databaseReference.child('Clients').once();
+    DatabaseEvent paymentsSnapshot = await _databaseReference.child('Rented').once();
+
+    List<Map<String, dynamic>> usersData = [];
+    List<Map<String, dynamic>> paymentsData = [];
+
+    if (usersSnapshot.snapshot.value != null) {
+      Map<dynamic, dynamic> usersMap = usersSnapshot.snapshot.value as Map<dynamic, dynamic>;
+      usersMap.forEach((key, value) {
+        usersData.add({
+          'name': value['name'],
+          'email': value['email'],
+        });
+      });
+    }
+
+    if (paymentsSnapshot.snapshot.value != null) {
+      Map<dynamic, dynamic> paymentsMap = paymentsSnapshot.snapshot.value as Map<dynamic, dynamic>;
+      paymentsMap.forEach((key, value) {
+        paymentsData.add({
+          'rented': value['rented'],
+          'price': value['price'],
+        });
+      });
+    }
+
+    setState(() {
+      users = usersData;
+      payments = paymentsData;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Payments and Users'),
+      ),
+      body: Column(
+        children: [
+          CupertinoSegmentedControl<bool>(
+            children: {
+              true: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Users'),
+              ),
+              false: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Payments'),
+              ),
+            },
+            onValueChanged: (bool value) {
+              setState(() {
+                showUsers = value;
+              });
+            },
+            groupValue: showUsers,
+          ),
+          Expanded(
+            child: showUsers ? _buildUserList() : _buildPaymentList(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildUserList() {
+    return ListView.builder(
+      itemCount: users.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(users[index]['name']),
+          subtitle: Text(users[index]['email']),
+        );
+      },
+    );
+  }
+
+  Widget _buildPaymentList() {
+    double total = payments.fold(0, (sum, item) => sum + item['price']);
+    return Column(
+      children: [
+        Expanded(
+          child: ListView.builder(
+            itemCount: payments.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(payments[index]['rented']),
+                subtitle: Text('Price: ${payments[index]['price']}'),
+              );
+            },
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text('Total: $total', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+        ),
+      ],
+    );
   }
 }
